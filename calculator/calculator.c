48aae852523e8831088be34d0c28d0b993265d28
JiScriptedHu <jayeshkumarnag1627@gmail.com>
2025-01-26 13:19:03 +0530

    Beginner's calculator

---
+++ calculator/calculator.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char optr;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter operation: ");
+    scanf(" %c", &optr);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    switch (optr)
+    {
+    case '+':
+        printf("Sum of %d and %d is %d", a, b, a+b);
+        break;
+    case '-':
+        printf("%d subtracted from %d is %d", b, a, a-b);
+        break;
+    case '*':
+        printf("Product of %d and %d is %d", a, b, a*b);
+        break;
+    case '/':
+        printf("%d divided by %d is %d", a, b, a/b);
+        break;
+    }
+
+    return 0;
+}
