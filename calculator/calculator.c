8da24331c0035a73a795bc87577858064f21ba95
JiScriptedHu <jayeshkumarnag1627@gmail.com>
2025-01-26 15:44:58 +0530

    Division with 0 and Data type handling

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
+    float a, b;
     char optr;
 
     printf("Enter first number: ");
-    scanf("%d", &a);
+    scanf("%f", &a);
 
     printf("Enter operation: ");
     scanf(" %c", &optr);
@@ -16,21 +16,26 @@ int main() {
     }
 
     printf("Enter second number: ");
-    scanf("%d", &b);
+    scanf("%f", &b);
 
     switch (optr)
     {
     case '+':
-        printf("Sum of %d and %d is %d", a, b, a+b);
+        printf("Sum of %.2f and %.2f is %.2f", a, b, a+b);
         break;
     case '-':
-        printf("%d subtracted from %d is %d", b, a, a-b);
+        printf("%.2f subtracted from %.2f is %.2f", b, a, a-b);
         break;
     case '*':
-        printf("Product of %d and %d is %d", a, b, a*b);
+        printf("Product of %.2f and %.2f is %.2f", a, b, a*b);
         break;
     case '/':
-        printf("%d divided by %d is %d", a, b, a/b);
+        if (b == 0) {
+            printf("Division with zero is not possible.");
+            return 1;
+        } else {
+            printf("%.2f divided by %.2f is %.2f", a, b, a/b);
+        }
         break;
     }
 
