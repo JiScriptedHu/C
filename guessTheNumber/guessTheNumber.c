bb05ab73987d495a0a5c6ce3478a5d77e58c78ac
JiScriptedHu <jayeshkumarnag1627@gmail.com>
2025-01-30 20:00:30 +0530

    Added Play Again or Exit choice

--- guessTheNumber/guessTheNumber.c
+++ guessTheNumber/guessTheNumber.c
@@ -4,25 +4,38 @@
 
 int input_guess();
 int check_guess(int number, int guess, int attempts);
+int exitMenu();
 
 int main() {
     int number, guess, attempts = 0;
+    char playAgain;
 
-    srand(time(0));
-    number = (rand() % 100) + 1;
+    
 
-    printf("Welcome to Guess The Number Game!\n");
-    printf("The number is between 1 and 100.\n");
+    do
+    {
+        srand(time(0));
+        number = (rand() % 100) + 1;
 
-    while (1){
-        guess = input_guess();
-        attempts++;
-        
-        if (check_guess(number, guess, attempts) == 1) {
-            break;
+        printf("\nWelcome to Guess The Number Game!\n");
+        printf("The number is between 1 and 100.\n");
+
+        while (1){
+            guess = input_guess();
+            attempts++;
+            
+            if (check_guess(number, guess, attempts) == 1) {
+                break;
+            }
+
         }
 
-    }
+        attempts = 0;
+        playAgain = exitMenu();
+
+    } while (playAgain == 1);
+
+    printf("Exiting the game...");
     
     return 0;
 }
@@ -52,5 +65,25 @@ int check_guess(int number, int guess, int attempts) {
     } else {
         printf("Congratulations! You guessed the correct number in %d attempts.\n", attempts);
         return 1;
+    }
+}
+
+int exitMenu() {
+    char exitChoice;
+    
+    while (1) {
+        printf("\nDo you want to play again? (y/n): ");
+        if (scanf(" %c", &exitChoice) == 1) {
+            if (exitChoice == 'y' || exitChoice == 'Y') {
+                return 1;
+            } else if (exitChoice == 'n' || exitChoice == 'N') {
+                return 0;
+            } else {
+                printf("\nInvalid input.");
+            }
+        } else {
+            printf("\nInvalid input.");
+            while (getchar() != '\n');
+        }
     }
 }
