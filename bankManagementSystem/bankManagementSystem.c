73e901d25bf9aa18289d3587d59282a1ab715c4e
JiScriptedHu <jayeshkumarnag1627@gmail.com>
2025-02-09 00:11:33 +0530

    Added Create and Search function

--- bankManagementSystem/bankManagementSystem.c
+++ bankManagementSystem/bankManagementSystem.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+struct account {
+    char fullName[50];
+    int age;
+    long long contactNo;
+    long long accountNo;
+};
+
+struct account accounts[100];
+int accountCount = 0;
+
 void createAccount();
 void searchAccount();
 
@@ -20,19 +30,58 @@ int main() {
         } else if (menuInput == 2) {
             searchAccount();
         } else if (menuInput == 3) {
-            printf("Exited");
-            return 0;
+            printf("Exited\n");
+            break;
         } else {
             printf("Invalid input! Please try again.\n");
         }
     }
-    
 
     return 0;
 }
 
 void createAccount() {
+    if (accountCount >= 100) {
+        printf("Account limit reached.\n");
+        return;
+    }
+
+    struct account newAccount;
+    
+    printf("Enter Full Name: ");
+    scanf(" %[^\n]%*c", newAccount.fullName);
+    printf("Enter Age: ");
+    scanf("%d", &newAccount.age);
+    printf("Enter Contact Number: ");
+    scanf("%lld", &newAccount.contactNo);
+
+    newAccount.accountNo = 2512010000LL + accountCount;
+
+    accounts[accountCount] = newAccount;
+    accountCount++;
+
+    printf("Account created successfully. Account No: %lld\n", newAccount.accountNo);
 }
 
 void searchAccount() {
+    long long searchAccNo, found = 0;
+    printf("Enter Account Number to search: ");
+    scanf("%lld", &searchAccNo);
+
+
+    for (int i = 0; i < accountCount; i++) {
+        if (accounts[i].accountNo == searchAccNo) {
+            printf("\nAccount Found!\n");
+            printf("Full Name: %s\n", accounts[i].fullName);
+            printf("Age: %d\n", accounts[i].age);
+            printf("Contact No: %lld\n", accounts[i].contactNo);
+            printf("Account No: %lld\n", accounts[i].accountNo);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("Account not found.\n");
+    }
 }
